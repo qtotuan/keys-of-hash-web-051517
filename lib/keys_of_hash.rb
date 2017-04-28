99f8723d46ee9795b67906c4a6977280bd88a8ee
Quynh To Tuan <quynh.totuan@gmail.com>
2017-04-28 14:53:18 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,9 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    arr = []
+    each do |key, value|
+      arguments.include?(value) ? arr << key : nil
+    end
+    arr
   end
 end
